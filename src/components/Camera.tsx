2c7943eab674e7cfe0f33d525707384922bcf1c4
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2023-10-17 16:35:18 -0400

    Better handling of events

--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -2,65 +2,59 @@ import { useEffect, useRef, useState } from "react";
 import * as wasm from "../../solar_tools/pkg";
 
 export default function Camera() {
+  let myReq: number = 0;
+
+  // On hot module update, stop existing animations.
+  // Without this, animations stack.
+  if (import.meta.hot) {
+    import.meta.hot.on("vite:beforeUpdate", () => {
+      cancelAnimationFrame(myReq);
+    });
+  }
+
+  const outSize = 128;
+  const pxSize = 4;
+  const gap = 0;
+
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [media, setMedia] = useState<MediaStream | null>(null);
 
   async function getMediaStream() {
     const stream = await navigator.mediaDevices.getUserMedia({
-      video: true,
+      video: {
+        facingMode: { ideal: "environment" },
+      },
     });
 
     setMedia(stream);
   }
 
-  // If stream is ready, get it processed, then start draw loop
-  useEffect(() => {
-    let myReq: number = 0;
-    const outSize = 128;
-    const pxSize = 4;
-    const gap = 1;
+  function draw(
+    bufferVideo: HTMLVideoElement,
+    bufferCanvas: HTMLCanvasElement
+  ) {
+    const bufferCtx = bufferCanvas.getContext("2d");
+    bufferCtx?.drawImage(bufferVideo, 0, 0);
 
-    async function draw(
-      bufferVideo: HTMLVideoElement,
-      bufferCanvas: HTMLCanvasElement
-    ) {
-      // Wait for data to show up on video element
-      if (bufferVideo.videoHeight === 0 || bufferVideo.videoWidth === 0) {
-        myReq = requestAnimationFrame(
-          async () => await draw(bufferVideo, bufferCanvas)
-        );
-        return;
-      }
-      bufferCanvas.height = bufferVideo.videoHeight;
-      bufferCanvas.width = bufferVideo.videoWidth;
+    const size = Math.min(bufferVideo.videoHeight, bufferVideo.videoWidth);
+    const data = bufferCtx?.getImageData(
+      (bufferCanvas.width - size) / 2,
+      (bufferCanvas.height - size) / 2,
+      size,
+      size
+    );
 
-      const bufferCtx = bufferCanvas.getContext("2d");
-      if (bufferCtx === null) {
-        cancelAnimationFrame(myReq);
-        return;
-      }
-      bufferCtx.drawImage(bufferVideo, 0, 0);
-
-      const size = Math.min(bufferVideo.videoHeight, bufferVideo.videoWidth);
-      const data = bufferCtx.getImageData(
-        (bufferCanvas.width - size) / 2,
-        (bufferCanvas.height - size) / 2,
-        size,
-        size
-      );
-
+    if (data) {
       const processedData = wasm.process_img(data, outSize);
-
       const saturations = processedData.map((val) =>
-        Math.floor((val / 0xff) * 50)
+        Math.floor((val / 0xff) * 100)
       );
 
       const ctx = canvasRef.current?.getContext("2d");
-
       for (let row = 0; row < outSize; row++) {
         for (let col = 0; col < outSize; col++) {
           if (ctx) {
-            ctx.fillStyle = `hsl(50, 100%, ${
+            ctx.fillStyle = `hsl(70, 100%, ${
               saturations[row * outSize + col]
             }%)`;
           }
@@ -72,12 +66,17 @@ export default function Camera() {
           );
         }
       }
-
-      myReq = requestAnimationFrame(
-        async () => await draw(bufferVideo, bufferCanvas)
-      );
     }
 
+    myReq = requestAnimationFrame(() => draw(bufferVideo, bufferCanvas));
+
+    return () => {
+      cancelAnimationFrame(myReq);
+    };
+  }
+
+  // If stream is ready, get it processed, then start draw loop
+  useEffect(() => {
     if (media && canvasRef.current) {
       // Set canvas dimensions
       canvasRef.current.height = outSize * (pxSize + gap) - gap;
@@ -90,11 +89,18 @@ export default function Camera() {
 
       // Create dummy canvas element to draw stream to
       const bufferCanvas = document.createElement("canvas");
-      if (myReq === 0) {
-        myReq = requestAnimationFrame(
-          async () => await draw(bufferVideo, bufferCanvas)
-        );
-      }
+
+      bufferVideo.addEventListener(
+        "loadedmetadata",
+        () => {
+          bufferCanvas.height = bufferVideo.videoHeight;
+          bufferCanvas.width = bufferVideo.videoWidth;
+          myReq = requestAnimationFrame(async () =>
+            draw(bufferVideo, bufferCanvas)
+          );
+        },
+        { once: true }
+      );
     }
   }, [media, canvasRef]);
 
