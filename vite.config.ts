ba99515ab0e2af8bfe8a1eb36e6727f4f7ec8668
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2023-10-17 16:35:30 -0400

    Add SSL for testing purposes

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import topeLevelAwait from "vite-plugin-top-level-await";
@@ -5,5 +6,8 @@ import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm(), topeLevelAwait()],
+  plugins: [react(), wasm(), topeLevelAwait(), basicSsl()],
+  server: {
+    https: true,
+  },
 });
